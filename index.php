313580888782e843c07b6a5cbf1ff04cf308d788
Ezy93 <giordani001@gmail.com>
2022-04-19 15:59:20 +0200

    aggiunta la ristampa del paragrafo censurato e della sua lunghezza mantenendo quello originale

--- index.php
+++ index.php
@@ -18,14 +18,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
     <title> PHP badwords </title>
 </head>
 <body>
-    <p>
-        <?php echo str_replace(strtolower($badWord), "***", strtolower($paragraph)); ?>
-    </p>
-    <h3>
-        La lunghezza del paragrafo è: <?php echo strlen($paragraph);  ?>
-    </h3>
+    <header class="container d-flex flex-column align-items-center">
+        <div class="w-50 p-3 bg-primary">
+            <p class="my-4 fs-4">
+                <?php echo $paragraph; ?>
+            </p>
+            <h3 class="pt-5">
+                La lunghezza del paragrafo è: <?php echo strlen($paragraph);  ?>
+            </h3>
+        </div>
+        <div class="w-50 p-3 bg-secondary text-white">
+            <p class="my-4 fs-4">
+                <?php echo str_replace(strtolower($badWord), "***", strtolower($paragraph)); ?>
+            </p>
+            <h3 class="pt-5">
+                La nuova lunghezza del paragrafo è: <?php echo strlen(str_replace(strtolower($badWord), "***", strtolower($paragraph)));  ?>
+            </h3>
+        </div>
+    </header>
 </body>
 </html>
